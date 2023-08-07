e668ed1a97c8cead148fc16a5ac8a5620dbdc0a0
kieran-murphy <68723269+kieran-murphy@users.noreply.github.com>
2023-08-07 17:04:09 +1000

    Added basic user endpoint

---
+++ pages/api/review/index.js
@@ -0,0 +1,41 @@
+import { getShows, createShow, deleteShow } from "@/lib/prisma/shows";
+
+const handler = async (req, res) => {
+  if (req.method === "GET") {
+    try {
+      const { shows, error } = await getShows();
+      if (error) throw new Error(error);
+      return res.status(200).json({ shows });
+    } catch (error) {
+      return res.status(500).json({ error: error.message });
+    }
+  }
+
+  if (req.method === "POST") {
+    try {
+      const data = req.body.show;
+      const { show, error } = await createShow(data);
+      if (error) throw new Error(error);
+      return res.status(200).json({ show });
+    } catch (error) {
+      console.log(error);
+      return res.status(500).json({ error: error.message });
+    }
+  }
+
+  if (req.method === "DELETE") {
+    try {
+      const data = req.body.showID;
+      await deleteShow(data);
+      return res.status(200).json({ message: "Show deleted successfully" });
+    } catch (error) {
+      console.log(error);
+      return res.status(500).json();
+    }
+  }
+
+  res.setHeader("Allow", ["GET", "POST", "DELETE"]);
+  res.status(425).end(`Method ${req.method} is not allowed.`);
+};
+
+export default handler;
---
+++ pages/api/user/index.js
@@ -0,0 +1,41 @@
+import { getUsers, createUser, deleteUser } from "@/lib/prisma/users";
+
+const handler = async (req, res) => {
+  if (req.method === "GET") {
+    try {
+      const { users, error } = await getUsers();
+      if (error) throw new Error(error);
+      return res.status(200).json({ users });
+    } catch (error) {
+      return res.status(500).json({ error: error.message });
+    }
+  }
+
+  if (req.method === "POST") {
+    try {
+      const data = req.body.user;
+      const { user, error } = await createUser(data);
+      if (error) throw new Error(error);
+      return res.status(200).json({ user });
+    } catch (error) {
+      console.log(error);
+      return res.status(500).json({ error: error.message });
+    }
+  }
+
+  if (req.method === "DELETE") {
+    try {
+      const data = req.body.userID;
+      await deleteUser(data);
+      return res.status(200).json({ message: "User deleted successfully" });
+    } catch (error) {
+      console.log(error);
+      return res.status(500).json();
+    }
+  }
+
+  res.setHeader("Allow", ["GET", "POST", "DELETE"]);
+  res.status(425).end(`Method ${req.method} is not allowed.`);
+};
+
+export default handler;
