f9e9b5729e6c3b698ca64b6eff82bc221720e701
kieran-murphy <68723269+kieran-murphy@users.noreply.github.com>
2023-09-14 23:18:17 +1000

    Added POST, DELETE method to /api/show

--- pages/api/show/index.js
+++ pages/api/show/index.js
@@ -1,4 +1,4 @@
-import { getShows } from "@/lib/prisma/shows";
+import { getShows, createShow, deleteShow } from "@/lib/prisma/shows";
 
 const handler = async (req, res) => {
   if (req.method === "GET") {
@@ -11,31 +11,31 @@ const handler = async (req, res) => {
     }
   }
 
-  // if (req.method === "POST") {
-  //   try {
-  //     const data = req.body.show;
-  //     const { show, error } = await createShow(data);
-  //     if (error) throw new Error(error);
-  //     return res.status(200).json({ show });
-  //   } catch (error) {
-  //     console.log(error);
-  //     return res.status(500).json({ error: error.message });
-  //   }
-  // }
+  if (req.method === "POST") {
+    try {
+      const data = req.body.show;
+      const { show, error } = await createShow(data);
+      if (error) throw new Error(error);
+      return res.status(200).json({ show });
+    } catch (error) {
+      console.log(error);
+      return res.status(500).json({ error: error.message });
+    }
+  }
 
-  // if (req.method === "DELETE") {
-  //   try {
-  //     const data = req.body.showID;
-  //     await deleteShow(data);
-  //     return res.status(200).json({ message: "Show deleted successfully" });
-  //   } catch (error) {
-  //     console.log(error);
-  //     return res.status(500).json();
-  //   }
-  // }
+  if (req.method === "DELETE") {
+    try {
+      const data = req.body.showID;
+      const { show, error } = await deleteShow(data);
+      if (error) throw new Error(error);
+      return res.status(200).json({ show });
+    } catch (error) {
+      console.log(error);
+      return res.status(500).json();
+    }
+  }
 
-  // res.setHeader("Allow", ["GET", "POST", "DELETE"]);
-  res.setHeader("Allow", ["GET"]);
+  res.setHeader("Allow", ["GET", "POST", "DELETE"]);
   res.status(425).end(`Method ${req.method} is not allowed.`);
 };
 
