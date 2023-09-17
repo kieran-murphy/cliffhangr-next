cc8a07d12f53876f215a4c0114308b07ab9f272b
kieran-murphy <68723269+kieran-murphy@users.noreply.github.com>
2023-09-17 18:23:44 +1000

    Updated seed.js and added npm seed+delete commands

--- prisma/seed.js
+++ prisma/seed.js
@@ -3,7 +3,7 @@ const faker = require("faker");
 
 const prisma = new PrismaClient();
 
-const generatedUsernames = new Set(); // Use a Set to store usernames, ensuring uniqueness
+const generatedUsernames = new Set();
 
 const generateUniqueUsername = () => {
   let username;
@@ -14,17 +14,16 @@ const generateUniqueUsername = () => {
   return username;
 };
 
-const main = async () => {
-  const userCount = 100; // Or however many users you'd like to generate
+const populateUsers = async () => {
+  const userCount = 100;
   const users = [];
 
   for (let i = 0; i < userCount; i++) {
     const username = generateUniqueUsername();
-    const email = `${username}@cliffhangr.com`; // derive email from username
+    const email = `${username}@cliffhangr.com`;
     users.push({
       username: username,
-      email: email, // add the email to the user data
-      // ... other fields
+      email: email,
     });
   }
 
@@ -35,6 +34,200 @@ const main = async () => {
   } catch (error) {
     console.error("Error seeding the database:", error);
   }
+};
+
+const populateShows = async () => {
+  const titles = [
+    "breaking bad",
+    "ozark",
+    "better call saul",
+    "wandavision",
+    "lost",
+    "gossip girl",
+    "the office",
+    "brooklyn nine nine",
+    "my 600lb life",
+    "married at first site",
+  ];
+
+  try {
+    for (let title of titles) {
+      await prisma.show.create({
+        data: {
+          title: title,
+        },
+      });
+    }
+  } catch (error) {
+    console.error("Error seeding the database:", error);
+  }
+};
+
+const populateFavorites = async () => {
+  const userIds = [];
+  const showIds = [];
+
+  const users = await prisma.user.findMany();
+  const shows = await prisma.show.findMany();
+
+  users.map((user) => userIds.push(user.id));
+  shows.map((show) => showIds.push(show.id));
+
+  try {
+    for (let userId of userIds) {
+      for (let showId of showIds) {
+        const randomNumber = Math.floor(Math.random() * 6);
+        if (randomNumber > 4) {
+          await prisma.favoriteShow.create({
+            data: {
+              userId: userId,
+              showId: showId,
+            },
+          });
+        }
+      }
+    }
+  } catch (error) {
+    console.error("Error seeding the database:", error);
+  }
+};
+
+const populateReviews = async () => {
+  const userIds = [];
+  const showIds = [];
+
+  const users = await prisma.user.findMany();
+  const shows = await prisma.show.findMany();
+
+  users.map((user) => userIds.push(user.id));
+  shows.map((show) => showIds.push(show.id));
+
+  try {
+    for (let userId of userIds) {
+      for (let showId of showIds) {
+        const randomNumber = Math.floor(Math.random() * 5);
+        if (randomNumber > 3) {
+          const rating = Math.random() * 4 + 1;
+          await prisma.review.create({
+            data: {
+              userId: userId,
+              showId: showId,
+              // text: faker.word.words(10),
+              text: "This is a review",
+              rating: parseFloat(rating.toFixed(1)),
+            },
+          });
+        }
+      }
+    }
+  } catch (error) {
+    console.error("Error seeding the database:", error);
+  }
+};
+
+const populateComments = async () => {
+  const userIds = [];
+  const reviewIds = [];
+
+  const users = await prisma.user.findMany();
+  const reviews = await prisma.review.findMany();
+
+  users.map((user) => userIds.push(user.id));
+  reviews.map((review) => reviewIds.push(review.id));
+
+  try {
+    for (let userId of userIds) {
+      for (let reviewId of reviewIds) {
+        const randomNumber = Math.floor(Math.random() * 30);
+        if (randomNumber > 28) {
+          await prisma.commentOnReview.create({
+            data: {
+              userId: userId,
+              reviewId: reviewId,
+              // text: faker.word.words(10),
+              text: "This is a comment",
+            },
+          });
+        }
+      }
+    }
+  } catch (error) {
+    console.error("Error seeding the database:", error);
+  }
+};
+
+const populateReactOnReviews = async () => {
+  const userIds = [];
+  const reviewIds = [];
+  const reacts = ["LIKE", "LOVE", "LAUGH", "WOW", "ANGRY"];
+
+  const users = await prisma.user.findMany();
+  const reviews = await prisma.review.findMany();
+
+  users.map((user) => userIds.push(user.id));
+  reviews.map((review) => reviewIds.push(review.id));
+
+  try {
+    for (let userId of userIds) {
+      for (let reviewId of reviewIds) {
+        const randomNumber = Math.floor(Math.random() * 30);
+        const randomReact = reacts[Math.floor(Math.random() * reacts.length)];
+        if (randomNumber > 28) {
+          await prisma.reactOnReview.create({
+            data: {
+              userId: userId,
+              reviewId: reviewId,
+              react: randomReact,
+            },
+          });
+        }
+      }
+    }
+  } catch (error) {
+    console.error("Error seeding the database:", error);
+  }
+};
+
+const populateReactOnComments = async () => {
+  const userIds = [];
+  const commentIds = [];
+  const reacts = ["LIKE", "LOVE", "LAUGH", "WOW", "ANGRY"];
+
+  const users = await prisma.user.findMany();
+  const comments = await prisma.commentOnReview.findMany();
+
+  users.map((user) => userIds.push(user.id));
+  comments.map((comment) => commentIds.push(comment.id));
+
+  try {
+    for (let userId of userIds) {
+      for (let commentId of commentIds) {
+        const randomNumber = Math.floor(Math.random() * 50);
+        const randomReact = reacts[Math.floor(Math.random() * reacts.length)];
+        if (randomNumber > 48) {
+          await prisma.reactOnComment.create({
+            data: {
+              userId: userId,
+              commentId: commentId,
+              react: randomReact,
+            },
+          });
+        }
+      }
+    }
+  } catch (error) {
+    console.error("Error seeding the database:", error);
+  }
+};
+
+const main = async () => {
+  await populateUsers();
+  await populateShows();
+  await populateFavorites();
+  await populateReviews();
+  await populateReactOnReviews();
+  await populateComments();
+  await populateReactOnComments();
 };
 
 main()
