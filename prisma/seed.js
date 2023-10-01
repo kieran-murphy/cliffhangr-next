dff45232952d5b7d77110f478c676caa364f4dbe
kieran-murphy <68723269+kieran-murphy@users.noreply.github.com>
2023-10-01 23:47:00 +1000

    Added basic passwords to user

--- prisma/seed.js
+++ prisma/seed.js
@@ -21,9 +21,11 @@ const populateUsers = async () => {
   for (let i = 0; i < userCount; i++) {
     const username = generateUniqueUsername();
     const email = `${username}@cliffhangr.com`;
+    const password = faker.word.noun();
     users.push({
       username: username,
       email: email,
+      password: password,
     });
   }
 
---
+++ src/app/login/page.jsx
@@ -0,0 +1,80 @@
+"use client";
+
+import React, { useState } from "react";
+
+export default function Home() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    const formData = {
+      email: username + "@cliffhangr.com",
+      username: username,
+      password: password,
+    };
+
+    try {
+      const response = await fetch("http://localhost:3000/api/user", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ user: formData }),
+      });
+
+      const data = await response.json();
+      console.log(data);
+
+      if (response.ok) {
+        alert("Form data submitted successfully!");
+      } else {
+        alert("Submission failed: " + data.message);
+      }
+    } catch (error) {
+      console.error("There was an error submitting the form data", error);
+      alert("There was an error submitting the form data");
+    }
+  };
+
+  return (
+    <div className="App bg-gray-100 h-screen flex justify-center items-center">
+      <div className="bg-white p-8 rounded shadow-md w-96">
+        <h1 className="text-2xl mb-4 text-gray-600">Login</h1>
+        <form onSubmit={handleSubmit}>
+          <div className="mb-4">
+            <label className="block text-sm font-medium text-gray-600">
+              Username:
+            </label>
+            <input
+              type="text"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+              className="mt-1 p-2 w-full border rounded-md text-black"
+            />
+          </div>
+          <div className="mb-4">
+            <label className="block text-sm font-medium text-gray-600">
+              Password:
+            </label>
+            <input
+              type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              className="mt-1 p-2 w-full border rounded-md text-black"
+            />
+          </div>
+          <div>
+            <button
+              type="submit"
+              className="w-full bg-blue-500 text-white p-2 rounded hover:bg-blue-600"
+            >
+              Login
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+}
