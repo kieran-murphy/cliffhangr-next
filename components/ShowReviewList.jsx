01cf04023f3f381eb6c0ceb785240612f07cd9e7
kieran-murphy <68723269+kieran-murphy@users.noreply.github.com>
2024-09-23 21:03:45 +1000

    always show user's review first

--- components/ShowReviewList.jsx
+++ components/ShowReviewList.jsx
@@ -1,19 +1,29 @@
-import React from "react";
+import { useEffect, useState } from "react";
 import ShowReview from "@/components/ShowReview/ShowReview";
 
 const ShowReviewList = ({ user, show }) => {
+  const [sortedReviews, setSortedReviews] = useState([]);
+  useEffect(() => {
+    const sortingReviews = [...show.reviews].sort((a, b) => {
+      if (a.userId === user.id) return -1;
+      if (b.userId === user.id) return 1;
+      return 0;
+    });
+    setSortedReviews(sortingReviews);
+  }, [user, show]);
+
   return (
     <div className="my-6">
       <div className="flex flex-row place-content-between mx-2">
         <h1 className="text-2xl font-medium mt-4">Reviews</h1>
-        <h1 className="text-2xl font-medium mt-4">{show.reviews.length}</h1>
+        <h1 className="text-2xl font-medium mt-4">{sortedReviews.length}</h1>
       </div>
 
       <hr className="m-1 opacity-50" />
       <div>
         {show !== null &&
-          (show.reviews.length > 0 ? (
-            show.reviews.map((review, index) => (
+          (sortedReviews.length > 0 ? (
+            sortedReviews.map((review) => (
               <ShowReview
                 key={review.id}
                 user={user}
