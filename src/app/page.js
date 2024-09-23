d739d231eda3d83e62eba7eb77b60d3ccc9fc92e
kieran-murphy <68723269+kieran-murphy@users.noreply.github.com>
2024-09-23 21:49:44 +1000

    Added a basic homepage

--- src/app/page.js
+++ src/app/page.js
@@ -1,3 +1,41 @@
+"use client";
+
+import Link from "next/link";
+import { useSession } from "next-auth/react";
+
 export default function Home() {
-  return <h1>Hello world</h1>;
+  const { data: session } = useSession();
+  const user = session?.user || null; // Directly access user from session
+
+  return (
+    <div>
+      <h1 className="text-lg m-8">Welcome to cliffhangr!</h1>
+      <div className="mx-6 pt-10">
+        <Link href={`/show`}>
+          <label
+            htmlFor="my-modal"
+            className="btn btn-primary w-full mt-4 gap-2"
+          >
+            See Shows
+          </label>
+        </Link>
+        <Link href={`/user`}>
+          <label
+            htmlFor="my-modal"
+            className="btn btn-secondary w-full mt-4 gap-2"
+          >
+            See Users
+          </label>
+        </Link>
+        <Link href={`/user/${user.id}`}>
+          <label
+            htmlFor="my-modal"
+            className="btn btn-success w-full mt-4 gap-2"
+          >
+            See My Page
+          </label>
+        </Link>
+      </div>
+    </div>
+  );
 }
