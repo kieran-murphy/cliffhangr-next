de70e475dfed873738b1c9bbf3da99a9591421fb
kieran-murphy <68723269+kieran-murphy@users.noreply.github.com>
2024-09-30 23:08:47 +1000

    fix to homepage for first time loading

--- src/app/page.js
+++ src/app/page.js
@@ -5,37 +5,39 @@ import { useSession } from "next-auth/react";
 
 export default function Home() {
   const { data: session } = useSession();
-  const user = session?.user || null; // Directly access user from session
+  const user = session?.user || null;
 
-  return (
-    <div>
-      <h1 className="text-lg m-8">Welcome to cliffhangr!</h1>
-      <div className="mx-6 pt-10">
-        <Link href={`/show`}>
-          <label
-            htmlFor="my-modal"
-            className="btn btn-primary w-full mt-4 gap-2"
-          >
-            See Shows
-          </label>
-        </Link>
-        <Link href={`/user`}>
-          <label
-            htmlFor="my-modal"
-            className="btn btn-secondary w-full mt-4 gap-2"
-          >
-            See Users
-          </label>
-        </Link>
-        <Link href={`/user/${user.id}`}>
-          <label
-            htmlFor="my-modal"
-            className="btn btn-success w-full mt-4 gap-2"
-          >
-            See My Page
-          </label>
-        </Link>
+  if (user) {
+    return (
+      <div>
+        <h1 className="text-lg m-8">Welcome to cliffhangr!</h1>
+        <div className="mx-6 pt-10">
+          <Link href={`/show`}>
+            <label
+              htmlFor="my-modal"
+              className="btn btn-primary w-full mt-4 gap-2"
+            >
+              See Shows
+            </label>
+          </Link>
+          <Link href={`/user`}>
+            <label
+              htmlFor="my-modal"
+              className="btn btn-secondary w-full mt-4 gap-2"
+            >
+              See Users
+            </label>
+          </Link>
+          <Link href={`/user/${user.id}`}>
+            <label
+              htmlFor="my-modal"
+              className="btn btn-success w-full mt-4 gap-2"
+            >
+              See My Page
+            </label>
+          </Link>
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
