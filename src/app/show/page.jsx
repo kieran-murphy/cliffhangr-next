382b1112aebe28adb444e4140f91373bd30bfc92
kieran-murphy <68723269+kieran-murphy@users.noreply.github.com>
2024-11-18 18:34:13 +1000

    added pagination to shows list

--- src/app/show/page.jsx
+++ src/app/show/page.jsx
@@ -8,8 +8,11 @@ import ShowSearchBar from "@/components/ShowSearchBar";
 export default function Home() {
   const [shows, setShows] = useState([]);
   const [searchTerm, setSearchTerm] = useState("");
+  const [currentPage, setCurrentPage] = useState(1);
+  const showsPerPage = 8;
 
   useEffect(() => {
+    let isMounted = true;
     const getShows = async () => {
       const RequestOptions = {
         method: "GET",
@@ -30,21 +33,57 @@ export default function Home() {
         return error.message;
       }
     };
-
     const fetchShows = async () => {
       const shows = await getShows();
-      setShows(shows);
+      if (isMounted) {
+        setShows(shows);
+      }
     };
     fetchShows();
+    return () => {
+      isMounted = false;
+    };
   }, [searchTerm]);
+
+  const indexOfLastShow = currentPage * showsPerPage;
+  const indexOfFirstShow = indexOfLastShow - showsPerPage;
+  const currentShows = shows.slice(indexOfFirstShow, indexOfLastShow);
+
+  const paginate = (pageNumber) => setCurrentPage(pageNumber);
+  const nextPage = () => {
+    if (currentPage < Math.ceil(shows.length / showsPerPage)) {
+      setCurrentPage(currentPage + 1);
+    }
+  };
+  const prevPage = () => {
+    if (currentPage > 1) {
+      setCurrentPage(currentPage - 1);
+    }
+  };
 
   return (
     <>
       <ShowSearchBar setSearchTerm={setSearchTerm} />
       <div className="m-4">
-        {shows?.map((show) => {
-          return <ShowListItem key={show.id} show={show} />;
-        })}
+        {currentShows.map((show) => (
+          <ShowListItem key={show.id} show={show} />
+        ))}
+      </div>
+      <div className="flex flex-row place-content-between m-4">
+        <button
+          onClick={prevPage}
+          disabled={currentPage === 1}
+          className="btn mx-1"
+        >
+          {"< Prev"}
+        </button>
+        <button
+          onClick={nextPage}
+          disabled={currentPage === Math.ceil(shows.length / showsPerPage)}
+          className="btn mx-1"
+        >
+          {"Next >"}
+        </button>
       </div>
     </>
   );
