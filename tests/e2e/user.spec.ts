618c52abaf615b45e75b787fd341ebf50195a100
kieran-murphy <68723269+kieran-murphy@users.noreply.github.com>
2025-05-06 17:24:17 +1000

    Added Register User Playwright Test

--- tests/e2e/user.spec.ts
+++ tests/e2e/user.spec.ts
@@ -1,6 +1,7 @@
 import { test, expect } from '@playwright/test';
 import { goToProfilePage, login } from './utils/utils';
 import { testUser } from './data/testuser';
+import { faker } from "@faker-js/faker";
 
 test.describe('User Related Tests', () => {
   test('Username Change Test', async ({ page }) => {
@@ -22,5 +23,32 @@ test.describe('User Related Tests', () => {
     await page.getByRole('button', { name: 'Save' }).click();
     await expect(page.getByRole('heading', { name: originalUsername, exact: true })).toBeVisible();
     await expect(page.getByRole('button', { name: originalUsername, exact: true })).toBeVisible();
+  });
+
+  test('Register User Test', async ({ page }) => {
+    const newUsername = faker.internet.userName();
+    const newEmail = newUsername + '@cliffhangr.com';
+    const newPassword = faker.internet.password();
+    
+    await page.goto('/');
+
+    // Register a new user
+    await page.getByRole('link', { name: 'Register' }).click();
+    await page.locator('#username').click();
+    await page.locator('#username').fill(newUsername);
+    await page.locator('#email').click();
+    await page.locator('#email').fill(newEmail);
+    await page.locator('#password').click();
+    await page.locator('#password').fill(newPassword);
+    await page.locator('#confirmpassword').click();
+    await page.locator('#confirmpassword').fill(newPassword);
+    await page.getByRole('button', { name: 'Register' }).click();
+    
+    // Login with the new user
+    await page.goto('/');
+    await page.locator('#email').fill(newEmail);
+    await page.locator('#password').fill(newPassword);
+    await page.locator('button[type="submit"]').click();
+    await expect(page.getByRole('button', { name: newUsername })).toBeVisible();
   });
 });
