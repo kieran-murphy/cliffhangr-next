a46abbe539713004c22e116e471d80a9dc55a153
kieran-murphy <68723269+kieran-murphy@users.noreply.github.com>
2025-05-19 00:43:38 +1000

    Moved next-auth.d.ts to types folder

---
+++ types/next-auth.d.ts
@@ -0,0 +1,10 @@
+import { DefaultSession, DefaultUser } from "next-auth";
+
+declare module "next-auth" {
+  interface Session extends DefaultSession {
+    user: DefaultSession["user"] & { id: string };
+  }
+  interface User extends DefaultUser {
+    id: string; 
+  }
+}
