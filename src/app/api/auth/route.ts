c07143115e36e2d16b2074b0d2278346cca5bc43
kieran-murphy <68723269+kieran-murphy@users.noreply.github.com>
2025-05-23 23:43:12 +1000

    TypeScript update to api/auth/route

---
+++ src/app/api/auth/route.ts
@@ -0,0 +1,15 @@
+import { getServerSession } from "next-auth/next";
+import { authOptions } from "./[...nextauth]/route";
+import { NextRequest, NextResponse } from "next/server";
+
+import type { Session } from "next-auth";
+
+export async function GET(request: NextRequest): Promise<NextResponse> {
+  const session = (await getServerSession(authOptions)) as Session | null;
+
+  if (!session || !session.user) {
+    return NextResponse.json({ user: null }, { status: 401 });
+  }
+
+  return NextResponse.json({ user: session.user });
+}
