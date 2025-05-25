ed07ed6f3d2f756ca88177e53ff2b2351e491ded
kieran-murphy <68723269+kieran-murphy@users.noreply.github.com>
2025-05-25 23:48:40 +1000

    TypeScript update to Follow API functions

--- lib/prisma/follows.ts
+++ lib/prisma/follows.ts
@@ -1,12 +1,17 @@
 import prisma from "./index";
+import type { Follow } from "@prisma/client";
 
-export async function getFollows() {
+// Get all follows
+export async function getFollows(): Promise<
+  | { count: number; follows: Follow[] }
+  | { error: unknown }
+> {
   try {
     const followCount = await prisma.follow.count();
     const follows = await prisma.follow.findMany();
     return {
       count: followCount,
-      follows: follows,
+      follows,
     };
   } catch (error) {
     return { error };
@@ -15,29 +20,31 @@ export async function getFollows() {
   }
 }
 
-export const getFollow = async (followID) => {
+// Get a single follow by ID
+export async function getFollow(
+  followID: string
+): Promise<{ follow?: Follow; error?: string }> {
   try {
     const follow = await prisma.follow.findUnique({
-      where: {
-        id: followID,
-      },
+      where: { id: followID },
     });
     if (!follow) {
       return { error: "Follow not found." };
     }
     return { follow };
-  } catch (error) {
+  } catch (error: any) {
     return { error: error.message };
   } finally {
     await prisma.$disconnect();
   }
-};
+}
 
-export async function createFollow(follow) {
+// Create a follow
+export async function createFollow(
+  follow: Omit<Follow, "id">
+): Promise<{ follow?: Follow; error?: unknown }> {
   try {
-    const followFromDB = await prisma.follow.create({
-      data: follow,
-    });
+    const followFromDB = await prisma.follow.create({ data: follow });
     return { follow: followFromDB };
   } catch (error) {
     return { error };
@@ -46,16 +53,15 @@ export async function createFollow(follow) {
   }
 }
 
-export async function deleteFollow(followID) {
+// Delete a follow by ID
+export async function deleteFollow(
+  followID: string
+): Promise<{ follow?: Follow; error: string | null }> {
   try {
     const follow = await prisma.follow.delete({ where: { id: followID } });
+    return { follow, error: null };
+  } catch (error: any) {
     return {
-      follow: follow,
-      error: null,
-    };
-  } catch (error) {
-    return {
-      user: null,
       error: `Failed to delete follow with ID ${followID}: ${error.message}`,
     };
   }
