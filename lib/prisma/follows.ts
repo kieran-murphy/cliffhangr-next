852a20e0bfed455ee09bde277d9f60339e1dc1d9
kieran-murphy <68723269+kieran-murphy@users.noreply.github.com>
2025-05-27 00:21:05 +1000

    Fixes to lib/prisma/follows.ts

--- lib/prisma/follows.ts
+++ lib/prisma/follows.ts
@@ -2,10 +2,7 @@ import prisma from "./index";
 import type { Follow } from "@prisma/client";
 
 // Get all follows
-export async function getFollows(): Promise<
-  | { count: number; follows: Follow[] }
-  | { error: unknown }
-> {
+export async function getFollows(): Promise<{ count?: number; follows?: Follow[]; error?: string }> {
   try {
     const followCount = await prisma.follow.count();
     const follows = await prisma.follow.findMany();
@@ -14,7 +11,7 @@ export async function getFollows(): Promise<
       follows,
     };
   } catch (error) {
-    return { error };
+    return { error: error.message ?? "Failed to get follows" };
   } finally {
     await prisma.$disconnect();
   }
@@ -33,7 +30,7 @@ export async function getFollow(
     }
     return { follow };
   } catch (error: any) {
-    return { error: error.message };
+    return { error: error.message ?? "Failed to get follow" };
   } finally {
     await prisma.$disconnect();
   }
@@ -42,12 +39,12 @@ export async function getFollow(
 // Create a follow
 export async function createFollow(
   follow: Omit<Follow, "id">
-): Promise<{ follow?: Follow; error?: unknown }> {
+): Promise<{ follow?: Follow; error?: string }> {
   try {
     const followFromDB = await prisma.follow.create({ data: follow });
     return { follow: followFromDB };
   } catch (error) {
-    return { error };
+    return { error: error.message ?? "Failed to create follow" };
   } finally {
     await prisma.$disconnect();
   }
@@ -56,13 +53,13 @@ export async function createFollow(
 // Delete a follow by ID
 export async function deleteFollow(
   followID: string
-): Promise<{ follow?: Follow; error: string | null }> {
+): Promise<{ follow?: Follow; error?: string }> {
   try {
     const follow = await prisma.follow.delete({ where: { id: followID } });
-    return { follow, error: null };
+    return { follow };
   } catch (error: any) {
     return {
-      error: `Failed to delete follow with ID ${followID}: ${error.message}`,
+      error: `Failed to delete follow with ID ${followID}: ${error.message ?? "Unknown error"}`,
     };
   }
 }
