9dd3a25cb9dc3db16d33afa7c857fd74e594d09c
kieran-murphy <68723269+kieran-murphy@users.noreply.github.com>
2025-05-28 01:00:17 +1000

    TypeScript update to Reaction API functions

---
+++ lib/prisma/reactOnReviews.ts
@@ -0,0 +1,74 @@
+import prisma from "./index";
+
+import type { ReactOnReview } from "@prisma/client";
+
+// Get all reacts
+export async function getReactOnReviews(): Promise<{ count?: number; reactOnReviews?: ReactOnReview[]; error?: string }> {
+  try {
+    const reactOnReviewCount = await prisma.reactOnReview.count();
+    const reactOnReviews = await prisma.reactOnReview.findMany();
+    return {
+      count: reactOnReviewCount,
+      reactOnReviews: reactOnReviews,
+    };
+  } catch (error) {
+    return {
+      error: `Failed to get all reacts: ${error.message}`,
+    };
+  } finally {
+    await prisma.$disconnect();
+  }
+}
+
+// Get a single react by ID
+export const getReactOnReview = async (reactOnReviewID: string): Promise<{ reactOnReview?: ReactOnReview; error?: string }> => {
+  try {
+    const reactOnReview = await prisma.reactOnReview.findUnique({
+      where: {
+        id: reactOnReviewID,
+      },
+    });
+    if (!reactOnReview) {
+      return { error: `ReactOnReview with ID: ${reactOnReviewID} not found.` };
+    }
+    return { reactOnReview };
+  } catch (error) {
+    return {
+      error: `Failed to get react with ID ${reactOnReviewID}: ${error.message}`,
+    };  
+  } finally {
+    await prisma.$disconnect();
+  }
+};
+
+// Create a react
+export async function createReactOnReview(reactOnReview: Omit<ReactOnReview, "id">): Promise<{ reactOnReview?: ReactOnReview; error?: string }> {
+  try {
+    const reactOnReviewFromDB = await prisma.reactOnReview.create({
+      data: reactOnReview,
+    });
+    return { reactOnReview: reactOnReviewFromDB };
+  } catch (error) {
+    return {
+      error: `Failed to create react: ${error.message}`,
+    };
+  } finally {
+    await prisma.$disconnect();
+  }
+}
+
+// Delete a react by ID
+export async function deleteReactOnReview(reactOnReviewID: string): Promise<{ reactOnReview?: ReactOnReview; error?: string; }> {
+  try {
+    const reactOnReview = await prisma.reactOnReview.delete({
+      where: { id: reactOnReviewID },
+    });
+    return { reactOnReview };
+  } catch (error) {
+    return {
+      error: `Failed to delete reactOnReview with ID ${reactOnReviewID}: ${error.message}`,
+    };
+  } finally {
+    await prisma.$disconnect();
+  }
+}
